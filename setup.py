de149d1f977e260b5351b3aa4e3236ef6cf103c6
tuantong <76169085+tuantong@users.noreply.github.com>
2024-05-14 15:27:04 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="depth_anything",
-    version="0.1",
-    install_requires=["gradio_imageslider", "gradio==4.14.0", "torch", "torchvision", "opencv-python", "huggingface_hub"],
-    packages=find_packages(),
+    version="1.0",
+    install_requires=[],
+    packages=find_packages(exclude="notebooks"),
+    extras_require={
+        "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime", "gradio_imageslider", "gradio", "torch", "torchvision", "huggingface_hub"],
+        "dev": ["flake8", "isort", "black", "mypy"],
+    },
 )
