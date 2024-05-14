f752d015f789cdebe08fad034e12c00a10d84460
tuantong <76169085+tuantong@users.noreply.github.com>
2024-05-14 15:04:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="depth_anything",
     version="0.1",
-    install_requires=[gradio_imageslider, gradio==4.14.0, torch, torchvision, opencv-python, huggingface_hub],
+    install_requires=["gradio_imageslider", "gradio==4.14.0", "torch", "torchvision", "opencv-python", "huggingface_hub"],
     packages=find_packages(),
 )
